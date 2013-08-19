be8fe5f0fe635ac6ff465fb318039fee49a1582b
alexvpopov <alex.val.popov@gmail.com>
2013-08-19 14:28:42 +0300

    Finish chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,8 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign Up') }
-    it { should have_title(full_title('Sign Up')) }
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 
   describe "profile page" do
@@ -29,6 +29,18 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content("Name can't be blank")}
+        it { should have_content("Email can't be blank")}
+        it { should have_content('Email is invalid')}
+        it { should have_content('Password is too short (minimum is 6 characters)')}
+        it { should have_content("Password can't be blank")}
+      end
     end
 
     describe "with valid information" do
@@ -41,6 +53,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
