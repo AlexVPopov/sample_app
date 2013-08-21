fa5571651c368239392c12315bd5da65b386cf18
AlexVPopov <alex.val.popov@gmail.com>
2013-08-21 10:11:29 +0300

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@
   def new
   end
 
-	def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       sign_in user
       redirect_to user
     else
