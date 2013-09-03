f3f4a141ae93448e08ac6a55217c767cb7e5f958
Alex Popov <alex.val.popov@gmail.com>
2013-09-03 15:23:07 +0300

    Reach 9.3, all tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,13 +37,17 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
     # Before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
